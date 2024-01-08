ae3b2215ec5ae10ca414ecebc531fd6ffaa81fa8
philemon <philemonabakahmensah@gmail.com>
2024-01-08 11:37:22 +0000

    fix: fix error with the reaction Time being undefined

---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+    <div @click="stopTimer" v-if="showBlock" class="block">
+      click me
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['delay'],
+    data(){
+        return {
+            showBlock: false,
+            timer: null,
+            reactionTime: 0
+        }
+    },
+    mounted() {
+        console.log("mounted")
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer();
+            console.log(this.delay)
+        }, this.delay)
+    },
+    methods: {
+        startTimer(){
+            this.timer = setInterval(()=> {
+                this.reactionTime += 10
+            }, 10)
+        },
+        stopTimer(){
+            clearInterval(this.timer)
+            this.$emit('end', this.reactionTime)
+        }
+    }
+}
+</script>
+
+<style>
+.block{
+    width: 400px;
+    border-radius: 26px;
+    background: rgb(223, 223, 17);
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="resultsBlock">
+        <p>Reaction time {{ score }} ms</p>
+        <p class="rank">{{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score< 250){
+            this.rank = "Ninja Fingers"
+        }else if (this.score < 400){
+            this.rank = "Rapid Fingers"
+        }else{
+            this.rank = "Small pace"
+        }
+    }
+}
+</script>
+
+<style>
+
+
+.rank{
+    font-size: 1.4rem;
+    color: black;
+    font-weight: bold;
+}
+</style>
